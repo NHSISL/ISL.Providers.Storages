02047032025b41a2044e57f83f169d3f2cdac2ee
Christo du Toit <christo@dutoit.co.uk>
2024-09-04 17:24:01 +0100

    INFRA: Initial Check-In

---
+++ ISL.AzureBlobClient.Infrastructure/Program.cs
@@ -0,0 +1,20 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using ISL.AzureBlobClient.Infrastructure.Services;
+
+namespace ISL.AzureBlobClient.Infrastructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var scriptGenerationService = new ScriptGenerationService();
+
+            scriptGenerationService.GenerateBuildScript(
+                branchName: "main",
+                projectName: "ISL.AzureBlobClient");
+        }
+    }
+}
---
+++ ISL.AzureBlobClient.Tests.Acceptance/UnitTest1.cs
@@ -0,0 +1,15 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+namespace ISL.AzureBlobClient.Tests.Acceptance
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
