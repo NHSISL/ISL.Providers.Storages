b2354a6f37dc65788e2bda8f3e02bdad446ad927
Christo du Toit <christo@dutoit.co.uk>
2024-10-07 13:30:52 +0100

    BROKERS: Blob Storage Broker

--- ISL.Providers.Storages.Abstractions/IStoragesOperations.cs
+++ ISL.Providers.Storages.Abstractions/IStoragesOperations.cs
@@ -2,8 +2,26 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
 namespace ISL.Providers.Storages.Abstractions
 {
     public interface IStoragesOperations
-    { }
+    {
+        ValueTask CreateFileAsync(Stream input, string fileName, string container);
+        ValueTask RetrieveFileAsync(Stream output, string fileName, string container);
+        ValueTask DeleteFileAsync(string fileName, string container);
+        ValueTask<string> GetDownloadLinkAsync(string fileName, string container, DateTimeOffset expiresOn);
+
+        ValueTask CreateContainerAsync(string container);
+        ValueTask ListContainerAsync(string container);
+
+        ValueTask GetContainerACLAsync(string container);
+        ValueTask SetContainerACLAsync(string container);
+
+        ValueTask CreateAndAssignContainerRoleAsync(string container, string roleName);
+        ValueTask CreateAndAssignManagedIdentityToRoleAsync(string identity, string roleName);
+    }
 }
---
+++ ISL.Providers.Storages.AzureBlobStorage/Brokers/Storages/Blobs/BlobStorageBroker.cs
@@ -0,0 +1,45 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Net.Http;
+using System.Threading;
+using Azure;
+using Azure.Core.Pipeline;
+using Azure.Identity;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace ISL.Providers.Storages.AzureBlobStorage.Brokers.Storages.Blobs
+{
+    internal class BlobStorageBroker : IBlobStorageBroker
+    {
+        public BlobServiceClient blobServiceClient { get; private set; }
+
+        public BlobStorageBroker(string serviceUri, string azureTenantId)
+        {
+            var blobServiceClientOptions = new BlobClientOptions()
+            {
+                Transport = new HttpClientTransport(new HttpClient { Timeout = new TimeSpan(1, 0, 0) }),
+                Retry = { NetworkTimeout = new TimeSpan(1, 0, 0) },
+                EnableTenantDiscovery = true
+            };
+
+            this.blobServiceClient = new BlobServiceClient(
+                    serviceUri: new Uri(serviceUri),
+                    credential: new DefaultAzureCredential(
+                        new DefaultAzureCredentialOptions
+                        {
+                            VisualStudioTenantId = azureTenantId,
+                        }),
+                    options: blobServiceClientOptions);
+        }
+
+        public Response<UserDelegationKey> GetUserDelegationKey(
+            DateTimeOffset? startsOn,
+            DateTimeOffset expiresOn,
+            CancellationToken cancellationToken = default) =>
+            blobServiceClient.GetUserDelegationKey(DateTimeOffset.UtcNow, expiresOn, cancellationToken);
+    }
+}
