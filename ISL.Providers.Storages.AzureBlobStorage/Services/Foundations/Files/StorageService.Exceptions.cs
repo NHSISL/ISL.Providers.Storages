a9b328d4ac4e80e23536b1c2136a52737c0c74b4
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-10-09 13:29:49 +0100

    ShouldThrowDependencyExceptionOnCreateAsync -> PASS

--- ISL.Providers.Storages.AzureBlobStorage/Services/Foundations/Files/StorageService.Exceptions.cs
+++ ISL.Providers.Storages.AzureBlobStorage/Services/Foundations/Files/StorageService.Exceptions.cs
@@ -2,9 +2,12 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using Azure;
 using Azure.Identity;
 using ISL.Providers.Storages.AzureBlobStorage.Models.Foundations.Files.Exceptions;
+using Microsoft.WindowsAzure.Storage;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 using Xeptions;
 
@@ -32,8 +35,26 @@ namespace ISL.Providers.Storages.AzureBlobStorage.Services.Foundations.Storages
             {
                 throw await CreateDependencyValidationExceptionAsync(authenticationFailedException);
             }
-
-
+            catch (RequestFailedException requestFailedException)
+            {
+                throw await CreateDependencyExceptionAsync(requestFailedException);
+            }
+            catch (StorageException storageException)
+            {
+                throw await CreateDependencyExceptionAsync(storageException);
+            }
+            catch (OperationCanceledException operationCanceledException)
+            {
+                throw await CreateDependencyExceptionAsync(operationCanceledException);
+            }
+            catch (TimeoutException timeoutException)
+            {
+                throw await CreateDependencyExceptionAsync(timeoutException);
+            }
+            catch (IOException iOException)
+            {
+                throw await CreateDependencyExceptionAsync(iOException);
+            }
         }
 
         private async ValueTask<StorageValidationException> CreateValidationExceptionAsync(
@@ -54,6 +75,16 @@ namespace ISL.Providers.Storages.AzureBlobStorage.Services.Foundations.Storages
                 innerException: exception);
 
             return storageDependencyValidationException;
+        }
+
+        private async ValueTask<StorageDependencyException> CreateDependencyExceptionAsync(
+            Exception exception)
+        {
+            var storageDependencyException = new StorageDependencyException(
+                message: "Storage dependency error occurred, please fix errors and try again.",
+                innerException: exception);
+
+            return storageDependencyException;
         }
     }
 }
