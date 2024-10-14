3183590e39c6d0abae84b10a15b43420280747ec
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-10-14 13:45:20 +0100

    ShouldThrowDependencyValidationExceptionOnCreateContainerAsync -> PASS

---
+++ ISL.Providers.Storages.AzureBlobStorage.Tests.Unit/Services/Foundations/Files/StorageTests.Exceptions.CreateContainer.cs
@@ -0,0 +1,57 @@
+﻿using Azure.Storage.Blobs.Models;
+using FluentAssertions;
+using ISL.Providers.Storages.AzureBlobStorage.Models.Foundations.Files.Exceptions;
+using Moq;
+using System;
+using System.Threading.Tasks;
+
+namespace ISL.Providers.Storages.AzureBlobStorage.Tests.Unit.Services.Foundations.Files
+{
+    public partial class StorageTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationExceptionOnCreateContainerAsync(
+            Exception dependencyValidationException)
+        {
+            // given
+            string randomString = GetRandomString();
+            string someContainer = randomString;
+            string inputContainer = someContainer;
+
+            var failedStorageDependencyValidationException =
+                new FailedStorageDependencyValidationException(
+                    message: "Failed storage dependency validation error occurred, please contact support.",
+                    innerException: dependencyValidationException);
+
+            var expectedStorageDependencyValidationException =
+                new StorageDependencyValidationException(
+                    message: "Storage dependency validation error occurred, please fix errors and try again.",
+                    innerException: failedStorageDependencyValidationException);
+
+            this.blobServiceClientMock.Setup(client =>
+                client.CreateBlobContainerAsync(inputContainer, PublicAccessType.None, null, default))
+                    .Throws(dependencyValidationException);
+
+            // when
+            ValueTask createContainerTask =
+                this.storageService.CreateContainerAsync(inputContainer);
+
+            StorageDependencyValidationException actualStorageDependencyValidationException =
+                await Assert.ThrowsAsync<StorageDependencyValidationException>(testCode: createContainerTask.AsTask);
+
+            // then
+            actualStorageDependencyValidationException
+                .Should().BeEquivalentTo(expectedStorageDependencyValidationException);
+
+            this.blobServiceClientMock.Verify(client =>
+                client.CreateBlobContainerAsync(inputContainer, PublicAccessType.None, null, default),
+                    Times.Once);
+
+            this.blobServiceClientMock.VerifyNoOtherCalls();
+            this.blobContainerClientMock.VerifyNoOtherCalls();
+            this.blobClientMock.VerifyNoOtherCalls();
+            this.blobStorageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
