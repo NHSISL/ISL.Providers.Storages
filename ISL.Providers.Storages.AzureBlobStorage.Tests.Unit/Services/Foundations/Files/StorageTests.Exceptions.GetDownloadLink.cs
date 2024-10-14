64fb4d1e01e42323f7cd8119d0a00b2b243fd709
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-10-14 10:54:25 +0100

    ShouldThrowDependencyValidationExceptionOnGetDownloadLinkAsync -> FAIL

---
+++ ISL.Providers.Storages.AzureBlobStorage.Tests.Unit/Services/Foundations/Files/StorageTests.Exceptions.GetDownloadLink.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions;
+using ISL.Providers.Storages.AzureBlobStorage.Models.Foundations.Files.Exceptions;
+using Moq;
+using System;
+using System.Threading.Tasks;
+
+namespace ISL.Providers.Storages.AzureBlobStorage.Tests.Unit.Services.Foundations.Files
+{
+    public partial class StorageTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationExceptionOnGetDownloadLinkAsync(
+            Exception dependencyValidationException)
+        {
+            // given
+            string randomString = GetRandomString();
+            DateTimeOffset someDateTimeOffset = DateTimeOffset.UtcNow;
+            string someFileName = randomString;
+            string someContainer = randomString;
+            string inputFileName = someFileName;
+            string inputContainer = someContainer;
+            DateTimeOffset inputDateTimeOffset = someDateTimeOffset;
+
+            var failedStorageDependencyValidationException =
+                new FailedStorageDependencyValidationException(
+                    message: "Failed storage dependency validation error occurred, please contact support.",
+                    innerException: dependencyValidationException);
+
+            var expectedStorageDependencyValidationException =
+                new StorageDependencyValidationException(
+                    message: "Storage dependency validation error occurred, please fix errors and try again.",
+                    innerException: failedStorageDependencyValidationException);
+
+            this.blobServiceClientMock.Setup(client =>
+                client.GetBlobContainerClient(inputContainer))
+                    .Throws(dependencyValidationException);
+
+            // when
+            ValueTask<string> getDownloadLinkTask =
+                this.storageService.GetDownloadLinkAsync(inputFileName, inputContainer, inputDateTimeOffset);
+
+            StorageDependencyValidationException actualStorageDependencyValidationException =
+                await Assert.ThrowsAsync<StorageDependencyValidationException>(testCode: getDownloadLinkTask.AsTask);
+
+            // then
+            actualStorageDependencyValidationException
+                .Should().BeEquivalentTo(expectedStorageDependencyValidationException);
+
+            this.blobServiceClientMock.Verify(client =>
+                client.GetBlobContainerClient(inputContainer),
+                    Times.Once);
+
+            this.blobServiceClientMock.VerifyNoOtherCalls();
+            this.blobContainerClientMock.VerifyNoOtherCalls();
+            this.blobClientMock.VerifyNoOtherCalls();
+            this.blobStorageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
