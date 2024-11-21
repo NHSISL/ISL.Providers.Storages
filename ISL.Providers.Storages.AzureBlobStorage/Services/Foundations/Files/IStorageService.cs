dd4b0cbba24d7c97a434d8d74d1b4a024b692526
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-11-21 18:06:28 +0000

    CODE RUB: Code cleanup

--- ISL.Providers.Storages.AzureBlobStorage/Services/Foundations/Files/IStorageService.cs
+++ ISL.Providers.Storages.AzureBlobStorage/Services/Foundations/Files/IStorageService.cs
@@ -16,9 +16,8 @@ namespace ISL.Providers.Storages.AzureBlobStorage.Services.Foundations.Files
         ValueTask DeleteFileAsync(string fileName, string container);
         ValueTask<List<string>> ListFilesInContainerAsync(string container);
         ValueTask<string> GetDownloadLinkAsync(string fileName, string container, DateTimeOffset expiresOn);
-
         ValueTask CreateContainerAsync(string container);
-        ValueTask CreateAndAssignAccessPolicyToContainerAsync(string container, List<string> policyNames);
+        ValueTask CreateAndAssignAccessPoliciesToContainerAsync(string container, List<string> policyNames);
         ValueTask RemoveAccessPoliciesFromContainerAsync(string container);
     }
 }
