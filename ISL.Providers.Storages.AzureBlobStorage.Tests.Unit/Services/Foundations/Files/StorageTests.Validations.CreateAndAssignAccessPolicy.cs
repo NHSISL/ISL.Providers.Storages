e1c45c61f448abaac61221cd1846f1ab60896584
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-11-21 15:43:02 +0000

    ShouldThrowValidationExceptionOnCreateAccessPolicyIfPolicyNamesInvalidAsync -> FAIL

--- ISL.Providers.Storages.AzureBlobStorage.Tests.Unit/Services/Foundations/Files/StorageTests.Validations.CreateAndAssignAccessPolicy.cs
+++ ISL.Providers.Storages.AzureBlobStorage.Tests.Unit/Services/Foundations/Files/StorageTests.Validations.CreateAndAssignAccessPolicy.cs
@@ -53,5 +53,45 @@ namespace ISL.Providers.Storages.AzureBlobStorage.Tests.Unit.Services.Foundation
             this.blobStorageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnCreateAccessPolicyIfPolicyNamesInvalidAsync()
+        {
+            // given
+            string randomString = GetRandomString();
+            string inputContainer = randomString;
+
+            List<string> invalidPolicyNames = new List<string>
+            {
+                "reader",
+                "superuser"
+            };
+
+            var invalidPolicyNameStorageException =
+                new InvalidPolicyNameStorageException(
+                    message: "Invalid policy name, only reader and writer privileges are supported at this time.");
+
+            var expectedStorageValidationException =
+                new StorageValidationException(
+                    message: "Storage validation error occurred, please fix errors and try again.",
+                    innerException: invalidPolicyNameStorageException);
+
+            // when
+            ValueTask createAccessPolicyTask =
+                this.storageService.CreateAndAssignAccessPolicyToContainerAsync(inputContainer, invalidPolicyNames);
+
+            StorageValidationException actualStorageValidationException =
+                await Assert.ThrowsAsync<StorageValidationException>(testCode: createAccessPolicyTask.AsTask);
+
+            // then
+            actualStorageValidationException
+                .Should().BeEquivalentTo(expectedStorageValidationException);
+
+            this.blobServiceClientMock.VerifyNoOtherCalls();
+            this.blobContainerClientMock.VerifyNoOtherCalls();
+            this.blobClientMock.VerifyNoOtherCalls();
+            this.blobStorageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
