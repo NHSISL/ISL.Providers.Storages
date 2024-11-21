ae26a4b79e948b75764aa103681ea836fb50cd9d
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-11-21 17:04:41 +0000

    ShouldThrowServiceExceptionOnRemoveAccessPoliciesAsync -> PASS

--- ISL.Providers.Storages.AzureBlobStorage.Tests.Unit/Services/Foundations/Files/StorageTests.Exceptions.RemoveAccessPolicies.cs
+++ ISL.Providers.Storages.AzureBlobStorage.Tests.Unit/Services/Foundations/Files/StorageTests.Exceptions.RemoveAccessPolicies.cs
@@ -101,5 +101,49 @@ namespace ISL.Providers.Storages.AzureBlobStorage.Tests.Unit.Services.Foundation
             this.blobStorageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRemoveAccessPoliciesAsync()
+        {
+            // given
+            Exception someException = new Exception();
+            string randomString = GetRandomString();
+            string inputContainer = randomString;
+
+            var failedStorageServiceException =
+                new FailedStorageServiceException(
+                    message: "Failed storage service error occurred, please contact support.",
+                    innerException: someException);
+
+            var expectedStorageServiceException =
+                new StorageServiceException(
+                    message: "Storage service error occurred, please fix errors and try again.",
+                    innerException: failedStorageServiceException);
+
+            this.blobServiceClientMock.Setup(client =>
+                client.GetBlobContainerClient(inputContainer))
+                    .Throws(someException);
+
+            // when
+            ValueTask removeAccessPolicyTask =
+                this.storageService.RemoveAccessPoliciesFromContainerAsync(inputContainer);
+
+            StorageServiceException actualStorageServiceException =
+                await Assert.ThrowsAsync<StorageServiceException>(testCode: removeAccessPolicyTask.AsTask);
+
+            // then
+            actualStorageServiceException
+                .Should().BeEquivalentTo(expectedStorageServiceException);
+
+            this.blobServiceClientMock.Verify(client =>
+                client.GetBlobContainerClient(inputContainer),
+                    Times.Once);
+
+            this.blobServiceClientMock.VerifyNoOtherCalls();
+            this.blobContainerClientMock.VerifyNoOtherCalls();
+            this.blobClientMock.VerifyNoOtherCalls();
+            this.blobStorageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
