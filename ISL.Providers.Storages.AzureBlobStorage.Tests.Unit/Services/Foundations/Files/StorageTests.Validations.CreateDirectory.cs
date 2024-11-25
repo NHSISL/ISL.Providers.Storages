08f0989ae140c6935f9c74a42655230c498e7bec
Joe Parkes <joe.owens@answerdigital.com>
2024-11-25 11:59:31 +0000

    ShouldThrowValidationExceptionOnCreateDirectoryIfArgumentsInvalidAsync -> FAIL

---
+++ ISL.Providers.Storages.AzureBlobStorage.Tests.Unit/Services/Foundations/Files/StorageTests.Validations.CreateDirectory.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using ISL.Providers.Storages.AzureBlobStorage.Models.Foundations.Files.Exceptions;
+using System.Threading.Tasks;
+
+namespace ISL.Providers.Storages.AzureBlobStorage.Tests.Unit.Services.Foundations.Files
+{
+    public partial class StorageTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnCreateDirectoryIfArgumentsInvalidAsync(string invalidText)
+        {
+            // given
+            string invalidDirectory = invalidText;
+
+            var invalidArgumentStorageException =
+                new InvalidArgumentStorageException(
+                    message: "Invalid storage service argument(s), please fix the errors and try again.");
+
+            invalidArgumentStorageException.AddData(
+                key: "Directory",
+                values: "Text is invalid");
+
+            var expectedStorageValidationException =
+                new StorageValidationException(
+                    message: "Storage validation error occurred, please fix errors and try again.",
+                    innerException: invalidArgumentStorageException);
+
+            // when
+            ValueTask createDirectoryTask =
+                this.storageService.CreateDirectoryAsync(invalidDirectory);
+
+            StorageValidationException actualStorageValidationException =
+                await Assert.ThrowsAsync<StorageValidationException>(testCode: createDirectoryTask.AsTask);
+
+            // then
+            actualStorageValidationException
+                .Should().BeEquivalentTo(expectedStorageValidationException);
+
+            this.blobServiceClientMock.VerifyNoOtherCalls();
+            this.dataLakeFileSystemClientMock.VerifyNoOtherCalls();
+            this.blobContainerClientMock.VerifyNoOtherCalls();
+            this.blobClientMock.VerifyNoOtherCalls();
+            this.blobStorageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
