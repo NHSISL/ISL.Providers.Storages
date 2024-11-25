901e78d05a72362f8b027f43c840f7283c76ab9b
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-11-25 16:31:35 +0000

    WIP -> Setup service container

---
+++ ISL.Providers.Storages.AzureBlobStorage/Providers/AzureBlobStorage/AzureBlobStorageProvider.cs
@@ -0,0 +1,34 @@
+﻿using ISL.Providers.Storages.AzureBlobStorage.Brokers.Storages.Blobs;
+using ISL.Providers.Storages.AzureBlobStorage.Models;
+using ISL.Providers.Storages.AzureBlobStorage.Services.Foundations.Files;
+using ISL.Providers.Storages.AzureBlobStorage.Services.Foundations.Storages;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace ISL.Providers.Storages.AzureBlobStorage.Providers.AzureBlobStorage
+{
+    public class AzureBlobStorageProvider : IAzureBlobStorageProvider
+    {
+        private IStorageService storageService;
+        public AzureBlobStorageProvider(AzureBlobStoreConfigurations configurations)
+        {
+            IServiceProvider serviceProvider = RegisterServices(configurations);
+            InitializeClients(serviceProvider);
+        }
+
+        private void InitializeClients(IServiceProvider serviceProvider) =>
+            storageService = serviceProvider.GetRequiredService<IStorageService>();
+
+        private static IServiceProvider RegisterServices(AzureBlobStoreConfigurations configurations)
+        {
+            var serviceCollection = new ServiceCollection()
+                .AddTransient<IStorageService, StorageService>()
+                .AddTransient<IBlobStorageBroker, BlobStorageBroker>()
+                .AddSingleton(configurations);
+
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+            return serviceProvider;
+        }
+    }
+}
---
+++ ISL.Providers.Storages.AzureBlobStorage/Providers/AzureBlobStorage/IAzureBlobStorageProvider.cs
@@ -0,0 +1,7 @@
+﻿using ISL.Providers.Storages.Abstractions;
+
+namespace ISL.Providers.Storages.AzureBlobStorage.Providers.AzureBlobStorage
+{
+    public interface IAzureBlobStorageProvider : IStorageProvider
+    { }
+}
