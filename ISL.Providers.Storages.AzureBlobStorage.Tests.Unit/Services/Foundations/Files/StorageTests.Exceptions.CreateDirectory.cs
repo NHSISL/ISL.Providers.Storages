9f82904f550af59552a8c5a1231e9ec803135e89
Joe Parkes <joe.owens@answerdigital.com>
2024-11-25 14:31:26 +0000

    ShouldThrowDependencyExceptionOnCreateDirectoryAsync -> PASS

--- ISL.Providers.Storages.AzureBlobStorage.Tests.Unit/Services/Foundations/Files/StorageTests.Exceptions.CreateDirectory.cs
+++ ISL.Providers.Storages.AzureBlobStorage.Tests.Unit/Services/Foundations/Files/StorageTests.Exceptions.CreateDirectory.cs
@@ -55,5 +55,52 @@ namespace ISL.Providers.Storages.AzureBlobStorage.Tests.Unit.Services.Foundation
             this.blobClientMock.VerifyNoOtherCalls();
             this.blobStorageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [MemberData(nameof(DependencyExceptions))]
+        public async Task ShouldThrowDependencyExceptionOnCreateDirectoryAsync(Exception dependencyException)
+        {
+            // given
+            string randomContainerString = GetRandomString();
+            string inputContainer = randomContainerString;
+            string randomDirectoryString = GetRandomString();
+            string inputDirectory = randomDirectoryString;
+
+            var failedStorageDependencyException =
+                new FailedStorageDependencyException(
+                    message: "Failed storage dependency error occurred, please contact support.",
+                    innerException: dependencyException);
+
+            var expectedStorageDependencyException =
+                new StorageDependencyException(
+                    message: "Storage dependency error occurred, please fix errors and try again.",
+                    innerException: failedStorageDependencyException);
+
+            this.dataLakeServiceClientMock.Setup(client =>
+                client.GetFileSystemClient(inputContainer))
+                    .Throws(dependencyException);
+
+            // when
+            ValueTask createDirectoryTask =
+                this.storageService.CreateDirectoryAsync(inputContainer, inputDirectory);
+
+            StorageDependencyException actualStorageDependencyException =
+                await Assert.ThrowsAsync<StorageDependencyException>(testCode: createDirectoryTask.AsTask);
+
+            // then
+            actualStorageDependencyException
+                .Should().BeEquivalentTo(expectedStorageDependencyException);
+
+            this.dataLakeServiceClientMock.Verify(client =>
+                client.GetFileSystemClient(inputContainer),
+                    Times.Once);
+
+            this.blobServiceClientMock.VerifyNoOtherCalls();
+            this.dataLakeServiceClientMock.VerifyNoOtherCalls();
+            this.dataLakeFileSystemClientMock.VerifyNoOtherCalls();
+            this.blobContainerClientMock.VerifyNoOtherCalls();
+            this.blobClientMock.VerifyNoOtherCalls();
+            this.blobStorageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
