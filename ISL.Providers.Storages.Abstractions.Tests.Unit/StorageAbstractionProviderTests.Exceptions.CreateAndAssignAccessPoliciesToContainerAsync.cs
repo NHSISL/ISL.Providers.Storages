e04b3436b8bc1fb3214367c78d9cd4772e1198cd
Joe Parkes <joe.owens@answerdigital.com>
2024-11-26 15:41:09 +0000

    ShouldThrowDependencyValidationExceptionOnCreateAndAssignAccessPoliciesToContainerAsyncWhenTypeIStorageDependencyValidationException -> PASS

--- ISL.Providers.Storages.Abstractions.Tests.Unit/StorageAbstractionProviderTests.Exceptions.CreateAndAssignAccessPoliciesToContainerAsync.cs
+++ ISL.Providers.Storages.Abstractions.Tests.Unit/StorageAbstractionProviderTests.Exceptions.CreateAndAssignAccessPoliciesToContainerAsync.cs
@@ -51,5 +51,47 @@ namespace ISL.Providers.Storage.Abstractions.Tests.Unit
 
             this.storageProviderMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task
+            ShouldThrowDependencyValidationExceptionOnCreateAndAssignAccessPoliciesToContainerAsyncWhenTypeIStorageDependencyValidationException()
+        {
+            // given
+            var someException = new Xeption();
+
+            var someStorageValidationException =
+                new SomeStorageDependencyValidationException(
+                    message: "Some storage provider dependency validation exception occurred",
+                    innerException: someException,
+                    data: someException.Data);
+
+            StorageProviderDependencyValidationException expectedStorageValidationProviderException =
+                new StorageProviderDependencyValidationException(
+                    message: "Storage provider dependency validation errors occurred, please try again.",
+                    innerException: someStorageValidationException);
+
+            this.storageProviderMock.Setup(provider =>
+                provider.CreateAndAssignAccessPoliciesToContainerAsync(It.IsAny<List<string>>(), It.IsAny<string>()))
+                    .ThrowsAsync(someStorageValidationException);
+
+            // when
+            ValueTask createAndAssignAccessPoliciesToContainerAsyncTask =
+                this.storageAbstractionProvider
+                    .CreateAndAssignAccessPoliciesToContainerAsync(It.IsAny<List<string>>(), It.IsAny<string>());
+
+            StorageProviderDependencyValidationException actualStorageValidationProviderException =
+                await Assert.ThrowsAsync<StorageProviderDependencyValidationException>(
+                    testCode: createAndAssignAccessPoliciesToContainerAsyncTask.AsTask);
+
+            // then
+            actualStorageValidationProviderException.Should().BeEquivalentTo(
+                expectedStorageValidationProviderException);
+
+            this.storageProviderMock.Verify(provider =>
+                provider.CreateAndAssignAccessPoliciesToContainerAsync(It.IsAny<List<string>>(), It.IsAny<string>()),
+                    Times.Once);
+
+            this.storageProviderMock.VerifyNoOtherCalls();
+        }
     }
 }
---
+++ ISL.Providers.Storages.Abstractions/Models/Exceptions/StorageProviderDependencyValidationException.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using Xeptions;
+
+namespace ISL.Providers.Storages.Abstractions.Models.Exceptions
+{
+    public class StorageProviderDependencyValidationException : Xeption
+    {
+        public StorageProviderDependencyValidationException(string message, Xeption innerException)
+        : base(message, innerException)
+        { }
+
+        public StorageProviderDependencyValidationException(string message, Xeption innerException, IDictionary data)
+            : base(message, innerException, data)
+        { }
+    }
+}
