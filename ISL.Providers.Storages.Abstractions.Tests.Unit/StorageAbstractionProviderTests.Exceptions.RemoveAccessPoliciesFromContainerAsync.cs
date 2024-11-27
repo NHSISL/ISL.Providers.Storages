f824bf2aba04a6e285f2fa1de67a81e0de5700df
Joe Parkes <joe.owens@answerdigital.com>
2024-11-27 12:02:51 +0000

    ShouldThrowValidationExceptionOnRemoveAccessPoliciesFromContainerAsyncWhenTypeIStorageValidationException -> FAIL

---
+++ ISL.Providers.Storages.Abstractions.Tests.Unit/StorageAbstractionProviderTests.Exceptions.RemoveAccessPoliciesFromContainerAsync.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using ISL.Providers.Storages.Abstractions.Models.Exceptions;
+using ISL.Providers.Storages.Abstractions.Tests.Unit.Models.Exceptions;
+using Moq;
+using System.Threading.Tasks;
+using Xeptions;
+
+namespace ISL.Providers.Storage.Abstractions.Tests.Unit
+{
+    public partial class StorageAbstractionProviderTests
+    {
+        [Fact]
+        public async Task
+            ShouldThrowValidationExceptionOnRemoveAccessPoliciesFromContainerAsyncWhenTypeIStorageValidationException()
+        {
+            // given
+            var someException = new Xeption();
+
+            var someStorageValidationException =
+                new SomeStorageValidationException(
+                    message: "Some storage provider validation exception occurred",
+                    innerException: someException,
+                    data: someException.Data);
+
+            StorageProviderValidationException expectedStorageValidationProviderException =
+                new StorageProviderValidationException(
+                    message: "Storage provider validation errors occurred, please try again.",
+                    innerException: someStorageValidationException);
+
+            this.storageProviderMock.Setup(provider =>
+                provider.RemoveAccessPoliciesFromContainerAsync(It.IsAny<string>()))
+                    .ThrowsAsync(someStorageValidationException);
+
+            // when
+            ValueTask removeAccessPoliciesFromContainerTask =
+                this.storageAbstractionProvider
+                    .RemoveAccessPoliciesFromContainerAsync(It.IsAny<string>());
+
+            StorageProviderValidationException actualStorageValidationProviderException =
+                await Assert.ThrowsAsync<StorageProviderValidationException>(
+                    testCode: removeAccessPoliciesFromContainerTask.AsTask);
+
+            // then
+            actualStorageValidationProviderException.Should().BeEquivalentTo(
+                expectedStorageValidationProviderException);
+
+            this.storageProviderMock.Verify(provider =>
+                provider.RemoveAccessPoliciesFromContainerAsync(It.IsAny<string>()),
+                    Times.Once);
+
+            this.storageProviderMock.VerifyNoOtherCalls();
+        }
+    }
+}
