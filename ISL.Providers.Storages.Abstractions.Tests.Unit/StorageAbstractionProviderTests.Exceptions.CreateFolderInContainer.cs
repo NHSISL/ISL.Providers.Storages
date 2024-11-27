c250713805baf9e24aa3b9789ed41d8e3c984257
Joe Parkes <joe.owens@answerdigital.com>
2024-11-27 15:33:57 +0000

    ShouldThrowDependencyValidationExceptionOnCreateFolderInContainerAsyncWhenTypeIStorageDependencyValidationException -> PASS

--- ISL.Providers.Storages.Abstractions.Tests.Unit/StorageAbstractionProviderTests.Exceptions.CreateFolderInContainer.cs
+++ ISL.Providers.Storages.Abstractions.Tests.Unit/StorageAbstractionProviderTests.Exceptions.CreateFolderInContainer.cs
@@ -50,5 +50,47 @@ namespace ISL.Providers.Storage.Abstractions.Tests.Unit
 
             this.storageProviderMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task
+            ShouldThrowDependencyValidationExceptionOnCreateFolderInContainerAsyncWhenTypeIStorageDependencyValidationException()
+        {
+            // given
+            var someException = new Xeption();
+
+            var someStorageValidationException =
+                new SomeStorageDependencyValidationException(
+                    message: "Some storage provider dependency validation exception occurred",
+                    innerException: someException,
+                    data: someException.Data);
+
+            StorageProviderDependencyValidationException expectedStorageValidationProviderException =
+                new StorageProviderDependencyValidationException(
+                    message: "Storage provider dependency validation errors occurred, please try again.",
+                    innerException: someStorageValidationException);
+
+            this.storageProviderMock.Setup(provider =>
+                provider.CreateFolderInContainerAsync(It.IsAny<string>(), It.IsAny<string>()))
+                    .ThrowsAsync(someStorageValidationException);
+
+            // when
+            ValueTask createFolderInContainerAsyncTask =
+                this.storageAbstractionProvider
+                    .CreateFolderInContainerAsync(It.IsAny<string>(), It.IsAny<string>());
+
+            StorageProviderDependencyValidationException actualStorageValidationProviderException =
+                await Assert.ThrowsAsync<StorageProviderDependencyValidationException>(
+                    testCode: createFolderInContainerAsyncTask.AsTask);
+
+            // then
+            actualStorageValidationProviderException.Should().BeEquivalentTo(
+                expectedStorageValidationProviderException);
+
+            this.storageProviderMock.Verify(provider =>
+                provider.CreateFolderInContainerAsync(It.IsAny<string>(), It.IsAny<string>()),
+                    Times.Once);
+
+            this.storageProviderMock.VerifyNoOtherCalls();
+        }
     }
 }
