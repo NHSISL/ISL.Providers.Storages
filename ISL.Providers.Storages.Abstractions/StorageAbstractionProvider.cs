ba58f7f701010a6b51f70a8ce2ac44843145f9b6
Joe Parkes <joe.owens@answerdigital.com>
2024-11-27 11:54:38 +0000

    INIT: RemoveAccessPoliciesFromContainerAsync signatures

--- ISL.Providers.Storages.Abstractions/StorageAbstractionProvider.cs
+++ ISL.Providers.Storages.Abstractions/StorageAbstractionProvider.cs
@@ -191,5 +191,22 @@ namespace ISL.Providers.Storages.Abstractions
         {
             await this.storageProvider.CreateAndAssignAccessPoliciesToContainerAsync(container, policyNames);
         });
+
+        /// <summary>
+        /// Removes all stored access policies from the container.
+        /// </summary>
+        /// <param name="container">The name of the storage container.</param>
+        /// <returns>A <see cref="ValueTask"/> representing the asynchronous operation.</returns>
+        /// <exception cref="StorageValidationProviderException">
+        /// Thrown when validation of input parameters fails.
+        /// </exception>
+        /// <exception cref="StorageDependencyProviderException">
+        /// Thrown when there is an issue with the storage dependency.
+        /// </exception>
+        /// <exception cref="StorageServiceProviderException">
+        /// Thrown when there is a general issue in the storage service layer.
+        /// </exception>
+        public async ValueTask RemoveAccessPoliciesFromContainerAsync(string container) =>
+            throw new NotImplementedException();
     }
 }
