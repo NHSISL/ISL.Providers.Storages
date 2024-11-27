3f5ef4af7825e58466ded8235613279ba462fde8
Joe Parkes <joe.owens@answerdigital.com>
2024-11-27 17:01:07 +0000

    ShouldThrowUncatagorizedServiceExceptionOnCreateDirectorySasTokenAsyncWhenTypeIsNotExpected -> PASS

--- ISL.Providers.Storages.Abstractions.Tests.Unit/StorageAbstractionProviderTests.Exceptions.CreateDirectorySasTokenAsync.cs
+++ ISL.Providers.Storages.Abstractions.Tests.Unit/StorageAbstractionProviderTests.Exceptions.CreateDirectorySasTokenAsync.cs
@@ -182,5 +182,50 @@ namespace ISL.Providers.Storage.Abstractions.Tests.Unit
 
             this.storageProviderMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task
+            ShouldThrowUncatagorizedServiceExceptionOnCreateDirectorySasTokenAsyncWhenTypeIsNotExpected()
+        {
+            // given
+            var someException = new Xeption();
+
+            var uncatagorizedStorageProviderException =
+                new UncatagorizedStorageProviderException(
+                    message: "Storage provider not properly implemented. Uncatagorized errors found, " +
+                        "contact the storage provider owner for support.",
+                    innerException: someException,
+                    data: someException.Data);
+
+            StorageProviderServiceException expectedStorageServiceProviderException =
+                new StorageProviderServiceException(
+                    message: "Uncatagorized storage provider service error occurred, contact support.",
+                    innerException: uncatagorizedStorageProviderException);
+
+            this.storageProviderMock.Setup(provider =>
+                 provider.CreateDirectorySasTokenAsync(
+                     It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<DateTimeOffset>()))
+                    .ThrowsAsync(someException);
+
+            // when
+            ValueTask<string> createDirectorySasTokenTask =
+                this.storageAbstractionProvider.CreateDirectorySasTokenAsync(
+                    It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<DateTimeOffset>());
+
+            StorageProviderServiceException actualStorageServiceProviderException =
+                await Assert.ThrowsAsync<StorageProviderServiceException>(
+                    testCode: createDirectorySasTokenTask.AsTask);
+
+            // then
+            actualStorageServiceProviderException.Should().BeEquivalentTo(
+                expectedStorageServiceProviderException);
+
+            this.storageProviderMock.Verify(provider =>
+                provider.CreateDirectorySasTokenAsync(
+                    It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<DateTimeOffset>()),
+                        Times.Once);
+
+            this.storageProviderMock.VerifyNoOtherCalls();
+        }
     }
 }
