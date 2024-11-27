a99987a65d07337956f95d5f73d53be1825692cc
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-11-27 13:28:02 +0000

    ShouldThrowProviderValidationExceptionOnRemoveAccessPoliciesToContainerDependencyValidation -> FAIL

--- ISL.Providers.Storages.AzureBlobStorage.Tests.Unit/Providers/AzureBlobStorage/AzureBlobStorageTests.Exceptions.RemoveAccessPoliciesFromContainer.cs
+++ ISL.Providers.Storages.AzureBlobStorage.Tests.Unit/Providers/AzureBlobStorage/AzureBlobStorageTests.Exceptions.RemoveAccessPoliciesFromContainer.cs
@@ -2,6 +2,7 @@
 using ISL.Providers.Storages.AzureBlobStorage.Models.Foundations.Files.Exceptions;
 using ISL.Providers.Storages.AzureBlobStorage.Models.Providers.Exceptions;
 using Moq;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xeptions;
 
@@ -45,6 +46,52 @@ namespace ISL.Providers.Storages.AzureBlobStorage.Tests.Unit.Providers.AzureBlob
 
             this.storageServiceMock.Verify(service =>
                 service.RemoveAccessPoliciesFromContainerAsync(inputContainer),
+                    Times.Once);
+
+            this.storageServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task
+            ShouldThrowProviderValidationExceptionOnRemoveAccessPoliciesToContainerDependencyValidation()
+        {
+            // given
+            string randomContainer = GetRandomString();
+            List<string> randomPolicyNames = GetRandomStringList();
+            string inputContainer = randomContainer;
+            List<string> inputPolicyNames = randomPolicyNames;
+
+            var storageDependencyValidationException = new StorageDependencyValidationException(
+                message: "Storage dependency validation error occurred, please fix errors and try again.",
+                innerException: new Xeption());
+
+            var expectedAzureBlobStorageProviderValidationException =
+                new AzureBlobStorageProviderValidationException(
+                    message: "Azure blob storage provider dependency validation error occurred, " +
+                        "fix errors and try again.",
+                    innerException: (Xeption)storageDependencyValidationException.InnerException,
+                    data: storageDependencyValidationException.InnerException.Data);
+
+            this.storageServiceMock.Setup(service =>
+                service.CreateAndAssignAccessPoliciesToContainerAsync(inputContainer, inputPolicyNames))
+                    .ThrowsAsync(storageDependencyValidationException);
+
+            // when
+            ValueTask removeAccessPoliciesTask =
+                this.azureBlobStorageProvider.CreateAndAssignAccessPoliciesToContainerAsync(
+                    inputContainer, inputPolicyNames);
+
+            AzureBlobStorageProviderValidationException
+                actualAzureBlobStorageProviderValidationException =
+                    await Assert.ThrowsAsync<AzureBlobStorageProviderValidationException>(
+                        testCode: removeAccessPoliciesTask.AsTask);
+
+            // then
+            actualAzureBlobStorageProviderValidationException
+                .Should().BeEquivalentTo(expectedAzureBlobStorageProviderValidationException);
+
+            this.storageServiceMock.Verify(service =>
+                service.CreateAndAssignAccessPoliciesToContainerAsync(inputContainer, inputPolicyNames),
                     Times.Once);
 
             this.storageServiceMock.VerifyNoOtherCalls();
