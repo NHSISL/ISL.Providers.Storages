51504a67dc531d6b2849db84f3fac0e4ba8a7558
Joe Parkes <joe.owens@answerdigital.com>
2024-11-28 10:28:40 +0000

    ShouldThrowValidationExceptionOnRetrieveAllAccessPoliciesFromContainerAsyncWhenTypeIStorageValidationException -> PASS

--- ISL.Providers.Storages.Abstractions/StorageAbstractionProvider.cs
+++ ISL.Providers.Storages.Abstractions/StorageAbstractionProvider.cs
@@ -204,8 +204,11 @@ namespace ISL.Providers.Storages.Abstractions
         /// <exception cref="AzureBlobStorageProviderValidationException" />
         /// <exception cref="AzureBlobStorageProviderDependencyException" />
         /// <exception cref="AzureBlobStorageProviderServiceException" />
-        public async ValueTask<List<string>> RetrieveAllAccessPoliciesFromContainerAsync(string container) =>
-            await storageProvider.RetrieveAllAccessPoliciesFromContainerAsync(container);
+        public ValueTask<List<string>> RetrieveAllAccessPoliciesFromContainerAsync(string container) =>
+        TryCatch(async () =>
+        {
+            return await storageProvider.RetrieveAllAccessPoliciesFromContainerAsync(container);
+        });
 
 
         /// <summary>
