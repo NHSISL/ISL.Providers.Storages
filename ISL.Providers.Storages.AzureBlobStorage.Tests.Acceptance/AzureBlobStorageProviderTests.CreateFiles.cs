059e47bb673d87adc5b2cc39d8e9d64dd42e9b1c
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-11-29 14:05:58 +0000

    ShouldCreateFileAsync -> PASS

---
+++ ISL.Providers.Storages.AzureBlobStorage.Tests.Acceptance/AzureBlobStorageProviderTests.CreateFiles.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISL.Providers.Storages.AzureBlobStorage.Tests.Acceptance
+{
+    public partial class AzureBlobStorageProviderTests
+    {
+        [Fact]
+        public async Task ShouldCreateFileAsync()
+        {
+            // given
+            string randomFileName = GetRandomString();
+            randomFileName = randomFileName + ".csv";
+            string inputFileName = randomFileName;
+            string randomString = GetRandomString();
+            byte[] randomBytes = Encoding.UTF8.GetBytes(randomString);
+            string inputContainer = "testcontainer";
+            MemoryStream inputStream = new MemoryStream(randomBytes);
+            MemoryStream outputStream = new MemoryStream();
+            MemoryStream actualOutputStream = outputStream;
+
+            // when
+            await this.azureBlobStorageProvider.CreateFileAsync(inputStream, inputFileName, inputContainer);
+            await this.azureBlobStorageProvider.RetrieveFileAsync(actualOutputStream, inputFileName, inputContainer);
+
+            // then
+            actualOutputStream.Length.Should().BeGreaterThan(0);
+            await this.azureBlobStorageProvider.DeleteFileAsync(inputFileName, inputContainer);
+        }
+    }
+}
