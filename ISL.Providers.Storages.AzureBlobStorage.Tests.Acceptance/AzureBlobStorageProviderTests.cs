0225c4e97ec3d11e90de80778e95c54bb7c13187
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-11-29 10:23:10 +0000

    WIP

---
+++ ISL.Providers.Storages.AzureBlobStorage.Tests.Acceptance/AzureBlobStorageProviderTests.cs
@@ -0,0 +1,27 @@
+﻿using ISL.Providers.Storages.AzureBlobStorage.Models;
+using ISL.Providers.Storages.AzureBlobStorage.Providers.AzureBlobStorage;
+using Microsoft.Extensions.Configuration;
+
+namespace ISL.Providers.Storages.AzureBlobStorage.Tests.Acceptance
+{
+    public partial class AzureBlobStorageProviderTests
+    {
+        private readonly IAzureBlobStorageProvider azureBlobStorageProvider;
+        private readonly IConfiguration configuration;
+
+        public AzureBlobStorageProviderTests()
+        {
+            var configurationBuilder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            configuration = configurationBuilder.Build();
+
+            AzureBlobStoreConfigurations azureBlobStoreConfigurations = configuration
+                .GetSection("azureBlobStoreConfigurations").Get<AzureBlobStoreConfigurations>();
+
+            this.azureBlobStorageProvider = new AzureBlobStorageProvider(azureBlobStoreConfigurations);
+        }
+    }
+}
