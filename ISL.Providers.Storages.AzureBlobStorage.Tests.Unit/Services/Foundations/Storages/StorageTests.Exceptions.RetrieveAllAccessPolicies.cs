2066a6650c9e7b465c1eb2f69711bbe9a8460b8f
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-12-05 16:26:33 +0000

    ShouldThrowDependencyValidationExceptionOnRetrieveAllAccessPoliciesAndLogItAsync -> FAIL

---
+++ ISL.Providers.Storages.AzureBlobStorage.Tests.Unit/Services/Foundations/Storages/StorageTests.Exceptions.RetrieveAllAccessPolicies.cs
@@ -0,0 +1,61 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using FluentAssertions;
+using ISL.Providers.Storages.Abstractions.Models;
+using ISL.Providers.Storages.AzureBlobStorage.Models.Foundations.Files.Exceptions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ISL.Providers.Storages.AzureBlobStorage.Tests.Unit.Services.Foundations.Storages
+{
+    public partial class StorageTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationExceptionOnRetrieveAllAccessPoliciesAndLogItAsync(
+            Exception dependencyValidationException)
+        {
+            // given
+            string randomString = GetRandomString();
+            string someContainer = randomString;
+            string inputContainer = someContainer;
+
+            var failedStorageDependencyValidationException =
+                new FailedStorageDependencyValidationException(
+                    message: "Failed storage dependency validation error occurred, please contact support.",
+                    innerException: dependencyValidationException);
+
+            var expectedStorageDependencyValidationException =
+                new StorageDependencyValidationException(
+                    message: "Storage dependency validation error occurred, please fix errors and try again.",
+                    innerException: failedStorageDependencyValidationException);
+
+            this.blobStorageBrokerMock.Setup(broker =>
+                broker.GetBlobContainerClient(inputContainer))
+                    .Throws(dependencyValidationException);
+
+            // when
+            ValueTask<List<Policy>> retrieveAllAccessPoliciesTask =
+                this.storageService.RetrieveAllAccessPoliciesAsync(inputContainer);
+
+            StorageDependencyValidationException actualStorageDependencyValidationException =
+                await Assert.ThrowsAsync<StorageDependencyValidationException>(
+                    testCode: retrieveAllAccessPoliciesTask.AsTask);
+
+            // then
+            actualStorageDependencyValidationException
+                .Should().BeEquivalentTo(expectedStorageDependencyValidationException);
+
+            this.blobStorageBrokerMock.Verify(broker =>
+                broker.GetBlobContainerClient(inputContainer),
+                    Times.Once);
+
+            this.blobStorageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
