cb9073b5657cd5d04796cba9d5492502ea35a77d
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-12-05 12:07:03 +0000

    ShouldThrowValidationExceptionOnRetrieveAccessPolicyByNameIfArgumentsInvalidAsync -> FAIL

---
+++ ISL.Providers.Storages.AzureBlobStorage.Tests.Unit/Services/Foundations/Storages/StorageTests.Validations.RetrieveAccessPolicyByName.cs
@@ -0,0 +1,58 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using FluentAssertions;
+using ISL.Providers.Storages.Abstractions.Models;
+using ISL.Providers.Storages.AzureBlobStorage.Models.Foundations.Files.Exceptions;
+using System.Threading.Tasks;
+
+namespace ISL.Providers.Storages.AzureBlobStorage.Tests.Unit.Services.Foundations.Storages
+{
+    public partial class StorageTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnRetrieveAccessPolicyByNameIfArgumentsInvalidAsync(
+            string invalidText)
+        {
+            // given
+            string invalidContainer = invalidText;
+            string invalidPolicyName = invalidText;
+
+            var invalidArgumentStorageException =
+                new InvalidArgumentStorageException(
+                    message: "Invalid storage service argument(s), please fix the errors and try again.");
+
+            invalidArgumentStorageException.AddData(
+                key: "Container",
+                values: "Text is invalid");
+
+            invalidArgumentStorageException.AddData(
+                key: "PolicyName",
+                values: "Text is invalid");
+
+            var expectedStorageValidationException =
+                new StorageValidationException(
+                    message: "Storage validation error occurred, please fix errors and try again.",
+                    innerException: invalidArgumentStorageException);
+
+            // when
+            ValueTask<Policy> retrieveAllAccessPoliciesTask =
+                this.storageService.RetrieveAccessPolicyByNameAsync(invalidContainer, invalidPolicyName);
+
+            StorageValidationException actualStorageValidationException =
+                await Assert.ThrowsAsync<StorageValidationException>(
+                    testCode: retrieveAllAccessPoliciesTask.AsTask);
+
+            // then
+            actualStorageValidationException
+                .Should().BeEquivalentTo(expectedStorageValidationException);
+
+            this.blobStorageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
