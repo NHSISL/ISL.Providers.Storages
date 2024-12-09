700063364c01f3b088b33ab136fe53b1d76016e9
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-12-09 13:42:35 +0000

    ShouldThrowValidationExceptionOnDeleteContainerWhenTypeIsStorageValidationException -> FAIL

---
+++ ISL.Providers.Storages.Abstractions.Tests.Unit/StorageAbstractionProviderTests.Exceptions.DeleteContainer.cs
@@ -0,0 +1,56 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using FluentAssertions;
+using ISL.Providers.Storages.Abstractions.Models.Exceptions;
+using ISL.Providers.Storages.Abstractions.Tests.Unit.Models.Exceptions;
+using Moq;
+using System.Threading.Tasks;
+using Xeptions;
+
+namespace ISL.Providers.Storage.Abstractions.Tests.Unit
+{
+    public partial class StorageAbstractionProviderTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnDeleteContainerWhenTypeIsStorageValidationException()
+        {
+            // given
+            var someException = new Xeption();
+
+            var someStorageValidationException =
+                new SomeStorageValidationException(
+                    message: "Some storage provider validation exception occurred",
+                    innerException: someException,
+                    data: someException.Data);
+
+            StorageProviderValidationException expectedStorageValidationProviderException =
+                new StorageProviderValidationException(
+                    message: "Storage provider validation errors occurred, please try again.",
+                    innerException: someStorageValidationException);
+
+            this.storageProviderMock.Setup(provider =>
+                provider.DeleteContainerAsync(It.IsAny<string>()))
+                    .ThrowsAsync(someStorageValidationException);
+
+            // when
+            ValueTask deleteContainerTask =
+                this.storageAbstractionProvider
+                    .DeleteContainerAsync(It.IsAny<string>());
+
+            StorageProviderValidationException actualStorageValidationProviderException =
+                await Assert.ThrowsAsync<StorageProviderValidationException>(testCode: deleteContainerTask.AsTask);
+
+            // then
+            actualStorageValidationProviderException.Should().BeEquivalentTo(
+                expectedStorageValidationProviderException);
+
+            this.storageProviderMock.Verify(provider =>
+                provider.DeleteContainerAsync(It.IsAny<string>()),
+                    Times.Once);
+
+            this.storageProviderMock.VerifyNoOtherCalls();
+        }
+    }
+}
