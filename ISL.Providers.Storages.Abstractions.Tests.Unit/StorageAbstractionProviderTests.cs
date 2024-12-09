b80aff74929baf8c796db8143fcd698afc8a81e4
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-12-09 11:57:22 +0000

    ShouldRetrieveAllContainersAsync -> FAIL

--- ISL.Providers.Storages.Abstractions.Tests.Unit/StorageAbstractionProviderTests.cs
+++ ISL.Providers.Storages.Abstractions.Tests.Unit/StorageAbstractionProviderTests.cs
@@ -27,6 +27,27 @@ namespace ISL.Providers.Storage.Abstractions.Tests.Unit
         private static string GetRandomString() =>
             new MnemonicString().GetValue();
 
+        private static string GetRandomStringWithLengthOf(int length)
+        {
+            string result = new MnemonicString(wordCount: 1, wordMinLength: length, wordMaxLength: length).GetValue();
+
+            return result.Length > length ? result.Substring(0, length) : result;
+        }
+
+        private static List<string> GetRandomStringList()
+        {
+            int randomNumber = GetRandomNumber();
+            List<string> randomStringList = new List<string>();
+
+            for (int index = 0; index < randomNumber; index++)
+            {
+                string randomString = GetRandomStringWithLengthOf(randomNumber);
+                randomStringList.Add(randomString);
+            }
+
+            return randomStringList;
+        }
+
         private static int GetRandomNumber() =>
             new IntRange(max: 15, min: 2).GetValue();
 
