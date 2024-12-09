0af7e5dcf21d422aa6285637cbd2a1234f2fa5c0
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-12-09 13:16:52 +0000

    ShouldThrowValidationExceptionOnRetrieveAllContainersAsyncWhenTypeIsStorageValidationException -> FAIL

---
+++ ISL.Providers.Storages.Abstractions.Tests.Unit/StorageAbstractionProviderTests.Exceptions.RetrieveAllContainers.cs
@@ -0,0 +1,59 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using FluentAssertions;
+using ISL.Providers.Storages.Abstractions.Models.Exceptions;
+using ISL.Providers.Storages.Abstractions.Tests.Unit.Models.Exceptions;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xeptions;
+
+namespace ISL.Providers.Storage.Abstractions.Tests.Unit
+{
+    public partial class StorageAbstractionProviderTests
+    {
+        [Fact]
+        public async Task
+            ShouldThrowValidationExceptionOnRetrieveAllContainersAsyncWhenTypeIsStorageValidationException()
+        {
+            // given
+            var someException = new Xeption();
+
+            var someStorageValidationException =
+                new SomeStorageValidationException(
+                    message: "Some storage provider validation exception occurred",
+                    innerException: someException,
+                    data: someException.Data);
+
+            StorageProviderValidationException expectedStorageValidationProviderException =
+                new StorageProviderValidationException(
+                    message: "Storage provider validation errors occurred, please try again.",
+                    innerException: someStorageValidationException);
+
+            this.storageProviderMock.Setup(provider =>
+                provider.RetrieveAllContainersAsync())
+                    .ThrowsAsync(someStorageValidationException);
+
+            // when
+            ValueTask<List<string>> retrieveAllContainersAsyncTask =
+                this.storageAbstractionProvider
+                    .RetrieveAllContainersAsync();
+
+            StorageProviderValidationException actualStorageValidationProviderException =
+                await Assert.ThrowsAsync<StorageProviderValidationException>(
+                    testCode: retrieveAllContainersAsyncTask.AsTask);
+
+            // then
+            actualStorageValidationProviderException.Should().BeEquivalentTo(
+                expectedStorageValidationProviderException);
+
+            this.storageProviderMock.Verify(provider =>
+                provider.RetrieveAllContainersAsync(),
+                    Times.Once);
+
+            this.storageProviderMock.VerifyNoOtherCalls();
+        }
+    }
+}
