c8e1ac27698dd6cd88aab71c227bc69e24c37662
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-12-09 10:24:33 +0000

    ShouldThrowProviderValidationExceptionOnRetrieveAllAccessPoliciesAsync -> FAIL

---
+++ ISL.Providers.Storages.AzureBlobStorage.Tests.Unit/Providers/AzureBlobStorage/AzureBlobStorageTests.Exceptions.RetrieveAllAccessPolicies.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using ISL.Providers.Storages.Abstractions.Models;
+using ISL.Providers.Storages.AzureBlobStorage.Models.Foundations.Files.Exceptions;
+using ISL.Providers.Storages.AzureBlobStorage.Models.Providers.Exceptions;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xeptions;
+
+namespace ISL.Providers.Storages.AzureBlobStorage.Tests.Unit.Providers.AzureBlobStorage
+{
+    public partial class AzureBlobStorageTests
+    {
+        [Fact]
+        public async Task ShouldThrowProviderValidationExceptionOnRetrieveAllAccessPoliciesAsync()
+        {
+            // given
+            string randomContainer = GetRandomString();
+            string inputContainer = randomContainer;
+
+            var storageValidationException = new StorageValidationException(
+                message: "Storage validation error occurred, please fix errors and try again.",
+                innerException: new Xeption());
+
+            var expectedAzureBlobStorageProviderValidationException =
+                new AzureBlobStorageProviderValidationException(
+                    message: "Azure blob storage provider validation error occurred, fix errors and try again.",
+                    innerException: (Xeption)storageValidationException.InnerException,
+                    data: storageValidationException.InnerException.Data);
+
+            this.storageServiceMock.Setup(service =>
+                service.RetrieveAllAccessPoliciesAsync(inputContainer))
+                    .ThrowsAsync(storageValidationException);
+
+            // when
+            ValueTask<List<Policy>> retrievePoliciesTask =
+                this.azureBlobStorageProvider.RetrieveAllAccessPoliciesAsync(inputContainer);
+
+            AzureBlobStorageProviderValidationException actualAzureBlobStorageProviderValidationException =
+                await Assert.ThrowsAsync<AzureBlobStorageProviderValidationException>(
+                    testCode: retrievePoliciesTask.AsTask);
+
+            // then
+            actualAzureBlobStorageProviderValidationException
+                .Should().BeEquivalentTo(expectedAzureBlobStorageProviderValidationException);
+
+            this.storageServiceMock.Verify(service =>
+                service.RetrieveAllAccessPoliciesAsync(inputContainer),
+                    Times.Once);
+
+            this.storageServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
