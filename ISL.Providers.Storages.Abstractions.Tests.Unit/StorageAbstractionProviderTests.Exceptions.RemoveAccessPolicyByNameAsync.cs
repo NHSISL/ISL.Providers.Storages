78fc248b56ea488f05eac347bbb1f1ad1f02c56f
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-12-10 16:12:57 +0000

    ShouldThrowValidationExceptionOnRemoveAccessPolicyByNameAsyncWhenTypeIsStorageValidationException -> PASS

---
+++ ISL.Providers.Storages.Abstractions.Tests.Unit/StorageAbstractionProviderTests.Exceptions.RemoveAccessPolicyByNameAsync.cs
@@ -0,0 +1,60 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using FluentAssertions;
+using ISL.Providers.Storages.Abstractions.Models.Exceptions;
+using ISL.Providers.Storages.Abstractions.Tests.Unit.Models.Exceptions;
+using Moq;
+using System.Threading.Tasks;
+using Xeptions;
+
+namespace ISL.Providers.Storage.Abstractions.Tests.Unit
+{
+    public partial class StorageAbstractionProviderTests
+    {
+        [Fact]
+        public async Task
+            ShouldThrowValidationExceptionOnRemoveAccessPolicyByNameAsyncWhenTypeIsStorageValidationException()
+        {
+            // given
+            var someException = new Xeption();
+
+            var someStorageValidationException =
+                new SomeStorageValidationException(
+                    message: "Some storage provider validation exception occurred",
+                    innerException: someException,
+                    data: someException.Data);
+
+            StorageProviderValidationException expectedStorageValidationProviderException =
+                new StorageProviderValidationException(
+                    message: "Storage provider validation errors occurred, please try again.",
+                    innerException: someStorageValidationException);
+
+            this.storageProviderMock.Setup(provider =>
+                provider.RemoveAccessPolicyByNameAsync(It.IsAny<string>(), It.IsAny<string>()))
+                    .ThrowsAsync(someStorageValidationException);
+
+            // when
+            ValueTask retrieveAccessPolicyByNameTask =
+                this.storageAbstractionProvider
+                    .RemoveAccessPolicyByNameAsync(It.IsAny<string>(), It.IsAny<string>());
+
+            StorageProviderValidationException actualStorageValidationProviderException =
+                await Assert.ThrowsAsync<StorageProviderValidationException>(
+                    testCode: retrieveAccessPolicyByNameTask.AsTask);
+
+            // then
+            actualStorageValidationProviderException.Should().BeEquivalentTo(
+                expectedStorageValidationProviderException);
+
+            this.storageProviderMock.Verify(provider =>
+                provider.RemoveAccessPolicyByNameAsync(It.IsAny<string>(), It.IsAny<string>()),
+                    Times.Once);
+
+            this.storageProviderMock.VerifyNoOtherCalls();
+        }
+
+
+    }
+}
