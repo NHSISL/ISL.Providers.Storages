1b5613e395ede7c41a07cec91751bb25e5acb956
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-12-16 15:13:24 +0000

    ShouldCreateSasTokenWithPermissionsListAsync -> PASS

--- ISL.Providers.Storages.AzureBlobStorage.Tests.Unit/Providers/AzureBlobStorage/AzureBlobStorageTests.Logic.CreateSasToken.cs
+++ ISL.Providers.Storages.AzureBlobStorage.Tests.Unit/Providers/AzureBlobStorage/AzureBlobStorageTests.Logic.CreateSasToken.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using Moq;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ISL.Providers.Storages.AzureBlobStorage.Tests.Unit.Providers.AzureBlobStorage
@@ -8,7 +9,7 @@ namespace ISL.Providers.Storages.AzureBlobStorage.Tests.Unit.Providers.AzureBlob
     public partial class AzureBlobStorageTests
     {
         [Fact]
-        public async Task ShouldCreateSasTokenAsync()
+        public async Task ShouldCreateSasTokenWithAccessPolicyAsync()
         {
             // given
             string randomContainer = GetRandomString();
@@ -48,6 +49,52 @@ namespace ISL.Providers.Storages.AzureBlobStorage.Tests.Unit.Providers.AzureBlob
                     inputPath,
                     inputAccessPolicyIdentifier,
                     inputExpiresOn),
+                        Times.Once);
+
+            this.storageServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldCreateSasTokenWithPermissionsListAsync()
+        {
+            // given
+            string randomContainer = GetRandomString();
+            string randomPath = GetRandomString();
+            DateTimeOffset randomDateTimeOffset = GetRandomFutureDateTimeOffset();
+            List<string> randomPermissionsList = GetRandomPermissionsList();
+            string randomSasToken = GetRandomString();
+            string inputPath = randomPath;
+            string inputContainer = randomContainer;
+            DateTimeOffset inputExpiresOn = randomDateTimeOffset;
+            List<string> inputPermissionsList = randomPermissionsList;
+            string outputSasToken = randomSasToken;
+            string expectedSasToken = outputSasToken;
+
+            this.storageServiceMock.Setup(service =>
+                service.CreateSasTokenAsync(
+                    inputContainer,
+                    inputPath,
+                    inputExpiresOn,
+                    inputPermissionsList))
+                        .ReturnsAsync(outputSasToken);
+
+            // when
+            string actualSasToken = await this.azureBlobStorageProvider
+                .CreateSasTokenAsync(
+                    inputContainer,
+                    inputPath,
+                    inputExpiresOn,
+                    inputPermissionsList);
+
+            // then
+            actualSasToken.Should().BeEquivalentTo(expectedSasToken);
+
+            this.storageServiceMock.Verify(service =>
+                service.CreateSasTokenAsync(
+                    inputContainer,
+                    inputPath,
+                    inputExpiresOn,
+                    inputPermissionsList),
                         Times.Once);
 
             this.storageServiceMock.VerifyNoOtherCalls();
